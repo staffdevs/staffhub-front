96d2b33a2c7c1b629948ecb5bb8b9f511d9cabbe
JuanZea <vikzea@gmail.com>
2023-04-15 11:35:25 -0500

    feat: initial commit

---
+++ src/App.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1>staffhub</h1>
+</template>
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import { createApp } from 'vue';
+import './css/main.css';
+import App from './App.vue';
+
+createApp(App).mount('#app');
