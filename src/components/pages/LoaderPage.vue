72dceb51640a6b0056cdc5009fe59a691710c179
JuanZea <vikzea@gmail.com>
2023-04-29 14:00:45 -0500

    Update LoaderPage.vue

--- src/components/pages/LoaderPage.vue
+++ src/components/pages/LoaderPage.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="flex h-screen items-center justify-center bg-yellowgreen bg-opacity-20">
-    <img src="src/assets/loader.svg" alt="a" />
+    <img src="/src/assets/loader.svg" alt="a" />
   </div>
 </template>
