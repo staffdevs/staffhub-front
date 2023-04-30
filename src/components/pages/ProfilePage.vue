adf56e0d913920d287a2dff08bc5f8e249ebbbb7
JuanZea <vikzea@gmail.com>
2023-04-29 23:32:50 -0500

    Update ProfilePage.vue

--- src/components/pages/ProfilePage.vue
+++ src/components/pages/ProfilePage.vue
@@ -1,14 +1,17 @@
 <script setup lang="ts">
 import { useAuth0 } from '@auth0/auth0-vue';
+import { ArrowLeftCircleIcon } from '@heroicons/vue/24/solid';
 
 const { user } = useAuth0();
 </script>
 
 <template>
-  <div class="bg-leaf flex h-screen items-center justify-center">
-    <div
-      class="flex flex-col items-center justify-center rounded-lg border-4 border-gray-700 bg-yellowgreen bg-opacity-70 p-8 drop-shadow-2xl"
-    >
+  <div class="bg-leaf flex h-screen flex-col items-center justify-center">
+    <router-link :to="{ name: 'home' }" class="flex items-center gap-2 rounded-t-xl bg-white px-8 py-1">
+      <ArrowLeftCircleIcon class="h-7 text-gray-700" />
+      <span>Regresar</span>
+    </router-link>
+    <div class="flex flex-col items-center justify-center rounded-lg border border-gray-200 bg-white p-8 drop-shadow">
       <img class="mx-auto my-4 h-40 rounded-full bg-white p-[1px]" :src="user.picture" alt="profile" />
       <h1 class="m-x-auto text-xl font-bold">
         {{ user.name }}
